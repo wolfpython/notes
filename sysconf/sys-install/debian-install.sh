ddab9193ad021bafa8298f21be6cc41be9dbc46f
wolf <wolf@debian.wolf.me>
2012-02-24 22:24:52 +0800

    add debian install

---
+++ sysconf/sys-install/debian-install.sh
@@ -0,0 +1,17 @@
+aptitude install build-essential
+
+aptitude install xserver-xorg-core xserver-xorg-input-{mouse,keyboard}
+
+aptitude install xserver-xorg-video-radeon xserver-xorg-video-nvidia
+
+aptitude install xterm rxvt-unicode-256color
+
+aptitude install vim vim-gtk emacs emacs-common-non-dfsg
+
+aptitude install geany e17
+
+aptitude install xarchiver mupdf iceweasel mplayer2 mpg123 gimp xchat
+
+aptitude install tcpdump wireshark
+
+aptitude install ddd stardict    abiword chromium filezilla fcitx git 
